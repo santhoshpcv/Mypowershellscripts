942d9386f01d4f3f345da76934329ad9b98f75b1
Santhosh cv <santhoshprashanth.cv@amadeus.com>
2020-06-15 13:54:19 +0530

    second script

---
+++ Rename list of vm/Rename-Vm.ps1
@@ -0,0 +1,9 @@
+﻿
+$vcenter = Read-Host "Enter the vCenter Name"
+connect-viserver $vcenter
+$capsvms = Get-Content C:\temp\vmlist.txt
+foreach($v in $capsvms) {
+
+$newname = $v.tolower()
+
+get-vm -name $vm | set-vm -name $newname -confirm:$false}
