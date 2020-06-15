be1721c9c6dd6470c6b44419f099e08c47c4e42d
Santhosh cv <santhoshprashanth.cv@amadeus.com>
2020-06-15 11:07:02 +0530

    this is first commit

---
+++ singleportgroup.ps1
@@ -0,0 +1,8 @@
+﻿
+$cluster=Get-Content C:\Users\pscv\Desktop\script\path.txt
+$vlan=Read-host "please enter the vlan"
+foreach($switch in $cluster){
+Get-vdswitch -Name "$switch"|New-VDPortgroup -name $switch-0$vlan -VlanId ([int]$vlan) -NumPorts 8 |Get-VDUplinkTeamingPolicy | Set-VDUplinkTeamingPolicy -LoadBalancingPolicy LoadBalanceLoadBased -enablefailback $false
+
+
+}
